dbb7f212d35b0de5fa4ed6733d2377ae629b6c4f
Michael-Susu12138 <78223776+Michael-Susu12138@users.noreply.github.com>
2023-12-04 21:31:07 -0500

    resolve cannot deploy error

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { AuthProvider } from "./context/AuthContext";
+// import { AuthProvider } from "./context/AuthContext";
 import "./App.css";
 import Home from "./pages/Home";
 import AddNews from "./pages/AddNews";
@@ -12,17 +12,17 @@ import Register from "./pages/Register";
 function App() {
   // console.log(process.env.REACT_APP_API);
   return (
-    <AuthProvider>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/add" element={<AddNews />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-        </Routes>
-      </Router>
-    </AuthProvider>
+    // <AuthProvider>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/add" element={<AddNews />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </Router>
+    // </AuthProvider>
   );
 }
 
